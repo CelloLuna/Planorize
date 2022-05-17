ceb1ce558d75a7215e17b003365f091a97f0b715
Luna <lunmars@dunwoody.edu>
2022-05-17 13:56:40 -0500

    cello's changes

--- tobbar.php
+++ tobbar.php
@@ -4,10 +4,9 @@
     <div class="container">
         <div class="navbar-header">
             <div class="btn-group">
-                <button class="btn btn-primary" data-calendar-nav="prev">
-                    << Prev</button>
-                        <button class="btn btn-default" data-calendar-nav="today">Today</button>
-                        <button class="btn btn-primary" data-calendar-nav="next">Next >></button>
+                <button class="btn btn-primary" data-calendar-nav="prev"><< Prev</button>
+                <button class="btn btn-default" data-calendar-nav="today">Today</button>
+                <button class="btn btn-primary" data-calendar-nav="next">Next >></button>
             </div>
             <div class="btn-group">
                 <button class="btn btn-warning" data-calendar-view="year">Year</button>
