ff85ee9d45bf0df28f2d645788cc69dcb4182d92
Caytlin <46500801+Caytlin@users.noreply.github.com>
2022-05-19 10:21:13 -0500

    Update profile.php

--- profile.php
+++ profile.php
@@ -24,17 +24,16 @@
 	            </div>
 	            <div class="card-body">
 	                <?php
-	                	#$id = isset($_GET['ID']) ?$_GET['ID'] : die("ERROR: ID not found.");
+	                	#$ID = isset($_GET['ID']) ?$_GET['ID'] : die("ERROR: User not found.");
 
 	                	#View the user's data
 	                	try
 	                	{
 
 	                		#Prepare select query
-		                	$query = "SELECT DateOfBirth, Email, FirstName, ID, LastName, Password, PhoneNumber, RegDate, TimeZone, Username From Users WHERE ID = ? LIMIT 0,1";
+		                	$query = "SELECT ID, FirstName, LastName, Password, Username, Email, PhoneNumber, DateOfBirth, TimeZone, RegDate, Status From users";
 
 		                	$stmt = $con -> prepare($query);
-		                	$stmt -> bindParam(1,$id);
 
 		                	#Execute the Query
 		                	$stmt -> execute();
@@ -43,15 +42,17 @@
 		                	$row = $stmt -> fetch(PDO::FETCH_ASSOC);
 
 		                	#Values to fill up
-		                	$DOB = $row["DateOfBirth"];
-		                	$Email = $row["Email"];
+		                	$Id = $row["ID"];
 		                	$FirstName = $row["FirstName"];
 		                	$LastName = $row["LastName"];
 		                	$Password = $row["Password"];
-		                	$Phone = $row["PhoneNumber"];
-		                	$TimeZone = $row["TimeZone"];
 		                	$Username = $row["Username"];
+		                	$Email = $row["Email"];
+		                	$Phone = $row["PhoneNumber"];
+		                	$DOB = $row["DateOfBirth"];
+		                	$TimeZone = $row["TimeZone"];
 		                	$RegDate = $row["RegDate"];
+		                	$Status = $row["Status"];
 
 	                	}
 	                	#Show any errors
@@ -108,22 +109,143 @@
 	            <div class="card-body">
 	                <div class="row">
 	                	<div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card shadow py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center" id="Amathyst">
+                                    	<a href onClick='ChangeTheme("./css/Amathyst.css")'><img src="./img/Amethyst.png" alt="Theme titled Amathyst. Theme is shades of purple" height="75" width="125"></a>
+                                    	<br>
+                                    	<figcaption style="text-align: center;">Amathyst</figcaption>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
                             <div class="card shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
-                                    	<a href="#">THEME PIC</a>
+                                    	<a href onClick='ChangeTheme("./css/Beloved.css")'><img src="./img/Beloved.png" alt="" height="75" width="125"></a>
                                     	<br>
-                                    	<p>THEME NAME</p>
+                                    	<figcaption style="text-align: center;">Beloved</figcaption>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                    	<a href onClick='ChangeTheme("./css/Blue-Green.css")'><img src="./img/Blue-Green.png" alt="" height="75" width="125"></a>
+                                    	<br>
+                                    	<figcaption style="text-align: center;">Blue-Green</figcaption>
                                     </div>
                                 </div>
                             </div>
                         </div>
 	                </div>
 	                <div class="row">
-	                	
+	                	<div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                    	<a href onClick='ChangeTheme("./css/CherryTomato.css")'><img src="./img/CherryTomato.png" alt="" height="75" width="125"></a>
+                                    	<br>
+                                    	<figcaption style="text-align: center;">Cherry Tomato</figcaption>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                    	<a href onClick='ChangeTheme("./css/Cotton_Candy.css")'><img src="./img/Cotton_Candy.png" alt="" height="75" width="125"></a>
+                                    	<br>
+                                    	<figcaption style="text-align: center;">Cotton Candy</figcaption>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                    	<a href onClick='ChangeTheme("./css/Dusk.css")'><img src="./img/Dusk.png" alt="" height="75" width="125"></a>
+                                    	<br>
+                                    	<figcaption style="text-align: center;">Dusk</figcaption>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 	                </div>
 	                <div class="row">
-	                	
+	                	<div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                    	<a href onClick='ChangeTheme("./css/Garnet.css")'><img src="./img/Garnet.png" alt="" height="75" width="125"></a>
+                                    	<br>
+                                    	<figcaption style="text-align: center;">Garnet</figcaption>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                    	<a href onClick='ChangeTheme("./css/LandSea.css")'><img src="./img/LandSea.png" alt="" height="75" width="125"></a>
+                                    	<br>
+                                    	<figcaption style="text-align: center;">Land and Sea</figcaption>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                    	<a href onClick='ChangeTheme("./css/Mystery.css")'><img src="./img/Mystery.png" alt="" height="75" width="125"></a>
+                                    	<br>
+                                    	<figcaption style="text-align: center;">Mystery</figcaption>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+	                </div>
+	                <div class="row">
+	                	<div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                    	<a href onClick='ChangeTheme("./css/Summer_Special.css")'><img src="./img/Summer_Special.png" alt="" height="75" width="125"></a>
+                                    	<br>
+                                    	<figcaption style="text-align: center;">Summer Special</figcaption>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                    	<a href onClick='ChangeTheme("./css/Sunshine.css")'><img src="./img/Sunshine.png" alt="" height="75" width="125"></a>
+                                    	<br>
+                                    	<figcaption style="text-align: center;">Sunshine</figcaption>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                    	<a href onClick='ChangeTheme("./css/Under_the_Waves.css")'><img src="./img/Under_the_Waves.png" alt="" height="75" width="125"></a>
+                                    	<br>
+                                    	<figcaption style="text-align: center;">Under The Waves</figcaption>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 	                </div>
 	            </div>
 	        </div>
@@ -142,6 +264,22 @@
         -->
 
     </div>
+    <script type="text/javascript">
+    	function ChangeTheme(){
+    		document.getElementById('Amathyst').setAttribute('href');
+    		document.getElementById('Beloved').setAttribute('href');
+    		document.getElementById('Blue-Green').setAttribute('href');
+    		document.getElementById('CherryTomato').setAttribute('href');
+    		document.getElementById('Cotton_Candy').setAttribute('href');
+    		document.getElementById('Dusk').setAttribute('href');
+    		document.getElementById('Garnet').setAttribute('href');
+    		document.getElementById('LandSea').setAttribute('href');
+    		document.getElementById('Mystery').setAttribute('href');
+    		document.getElementById('Summer_Special').setAttribute('href');
+    		document.getElementById('Sunshine').setAttribute('href');
+    		document.getElementById('Under_the_Waves').setAttribute('href');
+    	}
+    </script>
 
 <?php
 	//Include the footer
